6517675be41454a7371d33582fea64cecb4f5c57
alexander valdaitsevv <valdaitsevv@mail.ru>
2023-09-05 10:33:08 +0300

    я ебал рот

--- PWS_Lab3/PWS_Lab3/Controllers/PwsController.cs
+++ PWS_Lab3/PWS_Lab3/Controllers/PwsController.cs
@@ -15,6 +15,8 @@ using System.Web;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
+using System.Text;
+using System.ComponentModel;
 
 namespace PWS_Lab3.Controllers
 {
@@ -24,24 +26,12 @@ namespace PWS_Lab3.Controllers
 
 
         [HttpGet]
-        public async Task<IHttpActionResult> Get([FromUri] string contentType)
-        {
-            var students = await _repository.Students.ToListAsync();
-            var response = SetResponseContentType(students, contentType);
-            return Ok(students);
-        }
-
-        [HttpPost]
-        public async Task<IHttpActionResult> Post([FromBody] Student student, [FromUri] string contentType)
+        public async Task<HttpResponseMessage> Get([FromUri] [DefaultValue(null)] string contentType = null)
         {
             try
             {
-                var createdStudent = _repository.Students.Add(student);
-                await _repository.SaveChangesAsync();
-
-                var response = Request.CreateResponse(HttpStatusCode.OK, createdStudent);
-                SetResponseContentType(response.Content.Headers, contentType);
-
+                var students = await _repository.Students.ToListAsync();
+                var response = GetStudentsResponse(students, contentType);
                 return response;
             }
             catch (Exception ex)
@@ -50,73 +40,83 @@ namespace PWS_Lab3.Controllers
             }
         }
 
-        [HttpPut]
-        public async Task<IHttpActionResult> Put([FromBody] Student student, [FromUri] string contentType)
-        {
-            try
-            {
-                var studentToUpdate = await _repository.Students.Where(s => s.Id == student.Id).SingleOrDefaultAsync();
-            
-                studentToUpdate.Name = student.Name;
-                studentToUpdate.Phone = student.Phone;
-            
-                await _repository.SaveChangesAsync();
-
-                var response = Request.CreateResponse(HttpStatusCode.OK, studentToUpdate);
-                SetResponseContentType(response.Content.Headers, contentType);
-
-                return response;
-            }
-            catch (Exception ex)
-            {
-                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
-            }
-        }
-
-        [HttpDelete]
-        public async Task <IHttpActionResult> Delete([FromUri] int id, [FromUri] string contentType)
-        {
-            try
-            {
-                var studentToDelete = await _repository.Students.Where(s => s.Id == id).SingleOrDefaultAsync();
-                var deletedStudent = _repository.Students.Remove(studentToDelete);
-                await _repository.SaveChangesAsync();
-
-                var response = Request.CreateResponse(HttpStatusCode.OK, deletedStudent);
-                SetResponseContentType(response.Content.Headers, contentType);
-
-                return response;
-            }
-            catch (Exception ex)
-            {
-                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
-            }
-        }
-
-
-        //private void SetResponseContentType(HttpContentHeaders contentHeaders, string contentType, IEnumerable students)
+        //[HttpPost]
+        //public async Task<IHttpActionResult> Post([FromBody] Student student, [FromUri] string contentType)
         //{
-        //    contentHeaders.ContentType = new MediaTypeHeaderValue(
-        //        (contentType == "xml") ? "text/xml" : "application/json");
+        //    try
+        //    {
+        //        var createdStudent = _repository.Students.Add(student);
+        //        await _repository.SaveChangesAsync();
 
-        //    // To convert JSON text contained in string json into an XML node
-        //    XmlDocument doc = JsonConvert.DeserializeXmlNode(json);
+        //        var response = Request.CreateResponse(HttpStatusCode.OK, createdStudent);
+        //        SetResponseContentType(response.Content.Headers, contentType);
+
+        //        return response;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+        //    }
         //}
 
-        private HttpResponseMessage SetResponseContentType(IEnumerable<Student> students, string contentType)
+        //[HttpPut]
+        //public async Task<IHttpActionResult> Put([FromBody] Student student, [FromUri] string contentType)
+        //{
+        //    try
+        //    {
+        //        var studentToUpdate = await _repository.Students.Where(s => s.Id == student.Id).SingleOrDefaultAsync();
+            
+        //        studentToUpdate.Name = student.Name;
+        //        studentToUpdate.Phone = student.Phone;
+            
+        //        await _repository.SaveChangesAsync();
+
+        //        var response = Request.CreateResponse(HttpStatusCode.OK, studentToUpdate);
+        //        SetResponseContentType(response.Content.Headers, contentType);
+
+        //        return response;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+        //    }
+        //}
+
+        //[HttpDelete]
+        //public async Task <IHttpActionResult> Delete([FromUri] int id, [FromUri] string contentType)
+        //{
+        //    try
+        //    {
+        //        var studentToDelete = await _repository.Students.Where(s => s.Id == id).SingleOrDefaultAsync();
+        //        var deletedStudent = _repository.Students.Remove(studentToDelete);
+        //        await _repository.SaveChangesAsync();
+
+        //        var response = Request.CreateResponse(HttpStatusCode.OK, deletedStudent);
+        //        SetResponseContentType(response.Content.Headers, contentType);
+
+        //        return response;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+        //    }
+        //}
+
+
+        private HttpResponseMessage GetStudentsResponse(IEnumerable<Student> students, string contentType)
         {
             var response = new HttpResponseMessage();
 
             response.Content = (contentType == "xml") ?
-                 new StringContent(ConvertStudentsToXml(students), System.Text.Encoding.UTF8, "text/xml") :
-                 new StringContent(JsonConvert.SerializeObject(students), System.Text.Encoding.UTF8, "application/json");
+                 new StringContent(ConvertStudentsToXml(students), Encoding.UTF8, "text/xml") :
+                 new StringContent(JsonConvert.SerializeObject(students), Encoding.UTF8, "application/json");
 
             return response;
         }
 
         private string ConvertStudentsToXml(IEnumerable<Student> students)
         {
-            var serializer = new XmlSerializer(typeof(IEnumerable<Student>));
+            var serializer = new XmlSerializer(typeof(List<Student>));
             using (var writer = new StringWriter())
             {
                 serializer.Serialize(writer, students);

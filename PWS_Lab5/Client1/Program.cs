bdaa9021035e45cb5b13337304bc0070a9f81cf7
alexander valdaitsevv <valdaitsevv@mail.ru>
2024-01-05 17:44:09 +0300

    Add Lab5

---
+++ PWS_Lab5/Client1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Client1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client service1Client = new ServiceReference1.Service1Client();
+
+            var sumResult = service1Client.Sum(
+                new ServiceReference1.A { s = "hel", k = 1, f = 0.5f }, 
+                new ServiceReference1.A { s = "lo", k = 2, f = 1.5f }
+                );
+
+            Console.WriteLine($"======  SUM  ======");
+            Console.WriteLine($"\ns = {sumResult.s}\nf = {sumResult.f}\nk = {sumResult.k}\n\n");
+            Console.WriteLine($"======  ADD  =======");
+            Console.WriteLine($"\n{sumResult.k} + 10 = " + service1Client.Add(sumResult.k, 10) + "\n\n");
+            Console.WriteLine($"=====  CONCAT  =====");
+            Console.WriteLine($"\n{sumResult.s} + {sumResult.f} = " + service1Client.Concat(sumResult.s, sumResult.f) + "\n\n");
+
+            service1Client.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PWS_Lab5/Client2/Program.cs
@@ -0,0 +1,30 @@
+﻿using Client2.ServiceReference1;
+using System;
+using WCF;
+
+namespace Client2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client service1Client = new Service1Client();
+
+            var sumResult = service1Client.Sum(
+                new A { s = "wor", k = 2, f = 1.1f },
+                new A { s = "ld", k = 3, f = 1.4f }
+                );
+
+            Console.WriteLine($"======  SUM  ======");
+            Console.WriteLine($"\ns = {sumResult.s}\nf = {sumResult.f}\nk = {sumResult.k}\n\n");
+            Console.WriteLine($"======  ADD  =======");
+            Console.WriteLine($"\n{sumResult.k} + 10 = " + service1Client.Add(sumResult.k, 10) + "\n\n");
+            Console.WriteLine($"=====  CONCAT  =====");
+            Console.WriteLine($"\n{sumResult.s} + {sumResult.f} = " + service1Client.Concat(sumResult.s, sumResult.f) + "\n\n");
+
+            service1Client.Close();
+            
+            Console.ReadKey();
+        }
+    }
+}
